ecfb5d59d690c025caffc98e68eb9cd44b5893d8
lusterofgem <lusterofgem@gmail.com>
2021-10-20 01:13:41 +0800

    Test CMakeLists.txt with hello, world!

---
+++ build/CMakeFiles/english-helper.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/english-helper.dir/src/english-helper.cpp.o"
+  "CMakeFiles/english-helper.dir/src/english-helper.cpp.o.d"
+  "english-helper"
+  "english-helper.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/english-helper.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
