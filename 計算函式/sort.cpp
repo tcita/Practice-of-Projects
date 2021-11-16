d1b97ba1c51057732913009f11b70f92af32c4f2
tcita <50775529+tcita@users.noreply.github.com>
2021-11-16 19:51:32 +0800

    update io function

--- 計算函式/sort.cpp
+++ 計算函式/sort.cpp
@@ -1,25 +1,37 @@
-#include<vector>
+#include <deque>
 #include<iostream>
+#include <unordered_set>
 #include <unordered_map>
+#include<vector>
 #include <map>
 #include<string>
 #include<algorithm>
-
-#include<unordered_set>
+#include <fstream>
 using namespace std;
 
 
 class Solution {
 public:
-    //string paragraph:待處理的英文文章     vector<string>& banned 不納入統計的單字
-    void Wordfrequency(string paragraph, vector<string>& banned) {
+    string readfile() {
+        ifstream ifs("testfile.txt");
 
+        string content
+        ((istreambuf_iterator<char>(ifs)),
+            (istreambuf_iterator<char>()));
+        return  content;
+    }
+
+
+public:
+    //string paragraph:待處理的英文文章     vector<string>& banned 不納入統計的單字
+    string Wordfrequency(string paragraph, vector<string>& banned) {
+        string re;
         unordered_set<string> b;
-       
+
         //頻繁出現的衡量標準
         const int count = 3;
         //頻繁出現的單字
-        unordered_map<string,int> maxw;
+        unordered_map<string, int> maxw;
 
         //curr:當前考察的單字
         string curr;
@@ -27,8 +39,8 @@ public:
 
         //頻率的結果,使用map儲存後結果會按字母表排序
         map<string, int> str2cnt;
-        
-        
+
+
         for (string bb : banned)
         {
             b.insert(bb);
@@ -48,11 +60,7 @@ public:
                 {
                     ++str2cnt[curr];
 
-                    //檢查是否單字經常出現
-                    if (str2cnt[curr] >= count)
-                    {
-                        maxw.insert(pair<string, int>(curr,str2cnt[curr]));
-                    }
+
                 }
                 //結束了一個單字的解析,格式化curr為空
                 curr = "";
@@ -60,51 +68,69 @@ public:
         }
 
 
-         //如果文段的結尾單字沒有非字母結尾,如"I hate the sunlight" ,要額外討論,步驟只需要再重複上面一次即可
-        if (curr.size() > 0)
-        {
-            
-            if (b.find(curr) == b.end())
-            {
-                ++str2cnt[curr];
-                if (str2cnt[curr] >= count)
-                {
-                    maxw.insert(pair<string, int>(curr, str2cnt[curr]));
-                }
-            }
-            curr = "";
-        }
+        ////如果文段的結尾單字沒有非字母結尾,如"I hate the sunlight" ,要額外討論,步驟只需要再重複上面一次即可
+        //if (curr.size() > 0)
+        //{
 
-        
-        
-        
+        //    if (b.find(curr) == b.end())
+        //    {
+        //        ++str2cnt[curr];
+        //        if (str2cnt[curr] >= count)
+        //        {
+        //            maxw.insert(pair<string, int>(curr, str2cnt[curr]));
+        //        }
+        //    }
+        //    curr = "";
+        //}
+
+
+
+
         //結果輸出 
-        cout << "經常出現的單詞為 "<<"\n\n";
 
-              for (auto& t : maxw) {
+        //可能要改為需要的return 類型
 
-            cout << t.first << " " << t.second <<"次"<< endl;
+        cout << '\n' << "單字出現頻率\n";
 
-        }
-
-        cout << '\n'<<"單字出現頻率\n";
-        
         for (auto& t : str2cnt) {
 
-            cout << t.first << " " << t.second << "次" << endl;
+            //檢查是否單字經常出現
 
+            re += t.first + " " + to_string(t.second) + "次";
+            cout << t.first << " " << t.second << "次";
+
+            if (t.second >= count)
+            {
+
+                re += "▲";
+                cout << "▲";
+            }
+            re += "\n";
+            cout << endl;
         }
 
+        return re;
 
-       
     }
+    void write(string s) {
+        //不存在則新建
+        ofstream fout;
+        fout.open("out.txt");
+
+        fout << s;
+        fout.close();
+    }
+
+
 };
 int main() {
     //測試
-    vector<string>banned = {"hit"};
+    vector<string>banned = { "a","the","I" };
     Solution s;
-  s.Wordfrequency
-    ("Honeybees are characterised by their ability to produce liquefied sugar (honey) and a propensity to construct colonial nests using wax, two tasks that necessitate a significant level of social integration among members. As a result, they maintain strict divisions of labour, based on sex, with all males functioning as drones to fertilize and care for the eggs, and all females, with the exception of the single fertile queen, responsible for fetching nectar for the colony’s progeny. In addition, honeybees have devised a sophisticated system of communication to relay important information from member to member.", banned);
+    //s.Wordfrequency
+    //  ("Honeybees are characterised by their ability to produce liquefied sugar (honey) and a propensity to construct colonial nests using wax, two tasks that necessitate a significant level of social integration among members. As a result, they maintain strict divisions of labour, based on sex, with all males functioning as drones to fertilize and care for the eggs, and all females, with the exception of the single fertile queen, responsible for fetching nectar for the colony’s progeny. In addition, honeybees have devised a sophisticated system of communication to relay important information from member to member.", banned);
+    string str = s.readfile();
 
+    s.write(s.Wordfrequency(str, banned));
 
 }
