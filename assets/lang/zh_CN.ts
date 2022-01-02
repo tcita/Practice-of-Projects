f658f79eb5fc3a767e25122c54d157a45252f8ab
lusterofgem <lusterofgem@gmail.com>
2022-01-03 00:38:10 +0800

    Fix translation assets file

--- assets/lang/zh_CN.ts
+++ assets/lang/zh_CN.ts
@@ -11,37 +11,37 @@
     <message>
         <location filename="../../src/MainWindow.cpp" line="276"/>
         <source>English(US)</source>
-        <translation type="unfinished"></translation>
+        <translation>英文(美国)</translation>
     </message>
     <message>
         <location filename="../../src/MainWindow.cpp" line="277"/>
         <source>Chinese(China)</source>
-        <translation type="unfinished"></translation>
+        <translation>简体中文(中国)</translation>
     </message>
     <message>
         <location filename="../../src/MainWindow.cpp" line="278"/>
         <source>Chinese(Taiwan)</source>
-        <translation type="unfinished"></translation>
+        <translation>繁体中文(台湾)</translation>
     </message>
     <message>
         <location filename="../../src/MainWindow.cpp" line="280"/>
         <source>Main Panel</source>
-        <translation type="unfinished"></translation>
+        <translation>主页面</translation>
     </message>
     <message>
         <location filename="../../src/MainWindow.cpp" line="281"/>
         <source>Article Type Select Panel</source>
-        <translation type="unfinished"></translation>
+        <translation>文章选择页面</translation>
     </message>
     <message>
         <location filename="../../src/MainWindow.cpp" line="282"/>
         <source>Typing Panel</source>
-        <translation type="unfinished"></translation>
+        <translation>打字页面</translation>
     </message>
     <message>
         <location filename="../../src/MainWindow.cpp" line="283"/>
         <source>Translate Panel</source>
-        <translation type="unfinished"></translation>
+        <translation>翻译页面</translation>
     </message>
 </context>
 <context>
@@ -50,17 +50,17 @@
         <location filename="../../src/MainWindow.cpp" line="288"/>
         <location filename="../../src/MainWindow.cpp" line="289"/>
         <source>English Assistant</source>
-        <translation type="unfinished"></translation>
+        <translation>英语助手</translation>
     </message>
     <message>
         <location filename="../../src/MainWindow.cpp" line="290"/>
         <source>English translate &amp; learning program</source>
-        <translation type="unfinished"></translation>
+        <translation>英文翻译、学习软体</translation>
     </message>
     <message>
         <location filename="../../src/MainWindow.cpp" line="291"/>
         <source>Published under GPLv3</source>
-        <translation type="unfinished"></translation>
+        <translation>以GPLv3发布</translation>
     </message>
 </context>
 <context>
@@ -68,7 +68,7 @@
     <message>
         <location filename="../../src/MainWindow.cpp" line="270"/>
         <source>English Assistant</source>
-        <translation type="unfinished"></translation>
+        <translation>英语助手</translation>
     </message>
 </context>
 <context>
@@ -76,27 +76,27 @@
     <message>
         <location filename="../../src/MainWindow.cpp" line="274"/>
         <source>Setting</source>
-        <translation type="unfinished"></translation>
+        <translation>设定</translation>
     </message>
     <message>
         <location filename="../../src/MainWindow.cpp" line="275"/>
         <source>Language</source>
-        <translation type="unfinished"></translation>
+        <translation>语言</translation>
     </message>
     <message>
         <location filename="../../src/MainWindow.cpp" line="279"/>
         <source>Switch To</source>
-        <translation type="unfinished"></translation>
+        <translation>切换到</translation>
     </message>
     <message>
         <location filename="../../src/MainWindow.cpp" line="284"/>
         <source>Help</source>
-        <translation type="unfinished"></translation>
+        <translation>帮助</translation>
     </message>
     <message>
         <location filename="../../src/MainWindow.cpp" line="285"/>
         <source>About</source>
-        <translation type="unfinished"></translation>
+        <translation>关于</translation>
     </message>
 </context>
 <context>
@@ -104,52 +104,52 @@
     <message>
         <location filename="../../src/MainWindow.cpp" line="292"/>
         <source>OK</source>
-        <translation type="unfinished"></translation>
+        <translation>好的</translation>
     </message>
     <message>
         <location filename="../../src/MainWindow.cpp" line="295"/>
         <source>Article</source>
-        <translation type="unfinished"></translation>
+        <translation>文章</translation>
     </message>
     <message>
         <location filename="../../src/MainWindow.cpp" line="296"/>
         <source>Typing</source>
-        <translation type="unfinished"></translation>
+        <translation>打字</translation>
     </message>
     <message>
         <location filename="../../src/MainWindow.cpp" line="297"/>
         <source>Testing</source>
-        <translation type="unfinished"></translation>
+        <translation>测验</translation>
     </message>
     <message>
         <location filename="../../src/MainWindow.cpp" line="298"/>
         <source>Translate</source>
-        <translation type="unfinished"></translation>
+        <translation>翻译</translation>
     </message>
     <message>
         <location filename="../../src/MainWindow.cpp" line="301"/>
         <source>World</source>
-        <translation type="unfinished"></translation>
+        <translation>世界</translation>
     </message>
     <message>
         <location filename="../../src/MainWindow.cpp" line="302"/>
         <source>Business</source>
-        <translation type="unfinished"></translation>
+        <translation>商业</translation>
     </message>
     <message>
         <location filename="../../src/MainWindow.cpp" line="305"/>
         <source>Africa</source>
-        <translation type="unfinished"></translation>
+        <translation>非洲</translation>
     </message>
     <message>
         <location filename="../../src/MainWindow.cpp" line="308"/>
         <source>Submit</source>
-        <translation type="unfinished"></translation>
+        <translation>提交</translation>
     </message>
     <message>
         <location filename="../../src/MainWindow.cpp" line="311"/>
         <source>Testing Result:</source>
-        <translation type="unfinished"></translation>
+        <translation>测验结果:</translation>
     </message>
 </context>
 <context>
@@ -157,12 +157,12 @@
     <message>
         <location filename="../../src/MainWindow.cpp" line="314"/>
         <source>Source language</source>
-        <translation type="unfinished"></translation>
+        <translation>英文</translation>
     </message>
     <message>
         <location filename="../../src/MainWindow.cpp" line="315"/>
         <source>Dest language</source>
-        <translation type="unfinished"></translation>
+        <translation>简体中文</translation>
     </message>
 </context>
 </TS>
