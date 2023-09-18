4f8d2959d86255e1e4eeb2936d9a0c9d31f9c929
cpprust <cpprust@gmail.com>
2023-09-18 16:07:41 +0800

    Refector config namespace

--- src/english_assistance/config.cpp
+++ src/english_assistance/config.cpp
@@ -2,15 +2,17 @@
 #include "english_assistance/util.h"
 
 namespace english_assistance {
-    std::vector<std::string> config::readBannedWords() {
-        return util::split(util::readFile(BANNED_WORDS_PATH), '\n');
-    }
+    namespace config {
+        std::vector<std::string> readBannedWords() {
+            return util::split(util::readFile(BANNED_WORDS_PATH), '\n');
+        }
 
-    std::string config::readLanguageType() {
-        return util::readFile(LOCALE_PATH);
-    }
+        std::string readLanguageType() {
+            return util::readFile(LOCALE_PATH);
+        }
 
-    void config::writeLanguageType(const std::string &languageType) {
-        util::writeFile(LOCALE_PATH, languageType);
+        void writeLanguageType(const std::string &languageType) {
+            util::writeFile(LOCALE_PATH, languageType);
+        }
     }
 }
