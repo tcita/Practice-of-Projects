2080cae39c6d70b3fcec5d2e581508b2549df612
cpprust <cpprust@gmail.com>
2023-09-18 15:44:14 +0800

    Change constexpr string to inline static const string

--- include/english_assistance/lang.h
+++ include/english_assistance/lang.h
@@ -4,8 +4,8 @@
 
 namespace english_assistance {
     namespace lang {
-        constexpr std::string EN_US("en_US");
-        constexpr std::string ZH_CN("zh_CN");
-        constexpr std::string ZH_TW("zh_TW");
+        inline static const std::string EN_US("en_US");
+        inline static const std::string ZH_CN("zh_CN");
+        inline static const std::string ZH_TW("zh_TW");
     }
 }
