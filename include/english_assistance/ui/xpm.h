e5850a35274dda846c7f9c24e28ff764c0c4a091
cpprust <cpprust@gmail.com>
2023-09-18 12:32:57 +0800

    Remove unused xpm icon

--- include/english_assistance/ui/xpm.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-namespace english_assistance {
-    namespace ui {
-        namespace xpm {
-            extern const char *APP_ICON[];
-        }
-    }
-}
--- src/english_assistance/ui/main_window.cpp
+++ src/english_assistance/ui/main_window.cpp
@@ -11,7 +11,6 @@
 #include "english_assistance/translate.h"
 #include "english_assistance/ui/main_window.h"
 #include "english_assistance/util.h"
-#include "english_assistance/ui/xpm.h"
 
 namespace english_assistance {
     namespace ui {
