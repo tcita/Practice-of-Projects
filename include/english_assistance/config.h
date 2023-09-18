c1c0a653e2ff43587c676adcb95e7f24774f6330
cpprust <cpprust@gmail.com>
2023-09-18 16:15:11 +0800

    Move const path into config namespace

--- include/english_assistance/config.h
+++ include/english_assistance/config.h
@@ -5,11 +5,10 @@
 #include <vector>
 
 namespace english_assistance {
-    inline static const std::filesystem::path BANNED_WORDS_PATH = "config/banned_words.cfg";
-    inline static const std::filesystem::path LOCALE_PATH = "config/locale.cfg";
-
     // Get the config text file from project config folder
     namespace config {
+        inline static const std::filesystem::path BANNED_WORDS_PATH = "config/banned_words.cfg";
+        inline static const std::filesystem::path LOCALE_PATH = "config/locale.cfg";
         std::vector<std::string> readBannedWords();
         std::string readLanguageType();
         void writeLanguageType(const std::string &languageType);
