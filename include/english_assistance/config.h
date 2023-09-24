098a635327a010ecbb6debac439b11560d22baae
cpprust <cpprust@gmail.com>
2023-09-24 10:31:18 +0800

    Add a new line to seperate static const with function

--- include/english_assistance/config.h
+++ include/english_assistance/config.h
@@ -9,6 +9,7 @@ namespace english_assistance {
     namespace config {
         inline static const std::filesystem::path BANNED_WORDS_PATH = "config/banned_words.cfg";
         inline static const std::filesystem::path LOCALE_PATH = "config/locale.cfg";
+        
         std::vector<std::string> readBannedWords();
         std::string readLanguageType();
         void writeLanguageType(const std::string &languageType);
